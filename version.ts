fa366d33d6edc44313c82d44833aa5094eeaf3a2
ACB-Soft <aslancihatbasara@gmail.com>
2026-03-28 23:24:36 +0300

    refactor: Correct capitalization of brand name
    Update the `BRAND_NAME` constant from "İHA Plus" to "iHA Plus" to reflect the correct capitalization.

--- version.ts
+++ version.ts
@@ -1,4 +1,4 @@
 export const APP_VERSION = "v1.1";
-export const BRAND_NAME = "İHA Plus";
+export const BRAND_NAME = "iHA Plus";
 export const DEVELOPER = "ACB Software -";
 export const FULL_BRAND = `${DEVELOPER} ${BRAND_NAME} ${APP_VERSION}`;
